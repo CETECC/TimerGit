4e81166c11a38dee5df40e062d9add9c353d922d
marci <marci@DESKTOP-BTKP4J6>
2017-04-22 11:25:31 -0300

    Add project files.

---
+++ TimerGit/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimerGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
